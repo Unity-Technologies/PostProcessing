8713baa51e9810c04a21194f99fb3aa4660e4f56
Chman <chman.a11w@gmail.com>
2017-01-11 12:56:29 +0100

    Fixed GC alloc with OnGUI

--- PostProcessing/Runtime/PostProcessingBehaviour.cs
+++ PostProcessing/Runtime/PostProcessingBehaviour.cs
@@ -76,6 +76,8 @@ namespace UnityEngine.PostProcessing
 
             foreach (var component in m_Components)
                 m_ComponentStates.Add(component, false);
+
+            useGUILayout = false;
         }
 
         void OnPreCull()
