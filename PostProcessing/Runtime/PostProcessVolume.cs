f5b1e27cdad413655137f8b95ec14aec7901cc6b
Mike Geig <mikegeig@gmail.com>
2017-07-31 16:32:12 -0400

    Refactored gizmo code and implemented for the different colliders

--- PostProcessing/Runtime/PostProcessVolume.cs
+++ PostProcessing/Runtime/PostProcessVolume.cs
@@ -178,20 +178,15 @@ namespace UnityEngine.Rendering.PostProcessing
             }
 #endif
 
-           // var scale = transform.localScale;
-           // var invScale = new Vector3(1f / scale.x, 1f / scale.y, 1f / scale.z);
-            //Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, scale);
-
             // Draw a separate gizmo for each collider
             foreach (var collider in colliders)
             {
                 if (!collider.enabled)
                     continue;
 
-				var pos = collider.transform.position;
-				var lossyScale = collider.transform.lossyScale;
-				var scale = transform.localScale;
+				var scale = collider.transform.lossyScale;
 				var invScale = new Vector3(1f / scale.x, 1f / scale.y, 1f / scale.z);
+				Gizmos.matrix = Matrix4x4.TRS(collider.transform.position, collider.transform.rotation, scale);
 
                 // We'll just use scaling as an approximation for volume skin. It's far from being
                 // correct (and is completely wrong in some cases). Ultimately we'd use a distance
@@ -206,8 +201,8 @@ namespace UnityEngine.Rendering.PostProcessing
                 {
                     var c = (BoxCollider)collider;
 
-					Gizmos.DrawCube(pos + Vector3.Scale(c.center, scale), Vector3.Scale(lossyScale, c.size));
-                  //  Gizmos.DrawWireCube(c.center, c.size + invScale * blendDistance * 4f);
+					Gizmos.DrawCube(c.center, c.size);
+                	Gizmos.DrawWireCube(c.center, c.size + invScale * blendDistance * 4f);
                 }
                 else if (type == typeof(SphereCollider))
                 {
