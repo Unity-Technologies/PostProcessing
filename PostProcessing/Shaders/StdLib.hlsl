cbddfe8805acc65219c2e75c3824c745035e82d7
Robert Srinivasiah <robbies@gmail.com>
2017-08-23 01:40:24 -0700

    Remove comment

--- PostProcessing/Shaders/StdLib.hlsl
+++ PostProcessing/Shaders/StdLib.hlsl
@@ -242,10 +242,6 @@ VaryingsDefault VertDefault(AttributesDefault v)
     VaryingsDefault o;
     o.vertex = float4(v.vertex.xy, 0.0, 1.0);
     o.texcoord = TransformTriangleVertexToUV(v.vertex.xy);
-    // Most frag shaders that use texcoord do appear to use
-    // UnityStereoTransformScreenSpaceTex, but I need to
-    // manually inspect each shader.
-    // Seems like they'd be better off with the transform in the vert
 
 #if UNITY_UV_STARTS_AT_TOP
     o.texcoord = o.texcoord * float2(1.0, -1.0) + float2(0.0, 1.0);
