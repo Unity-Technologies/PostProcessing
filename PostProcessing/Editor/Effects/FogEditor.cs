6ca04028ced088b257f477e07043dd2f1a28dd88
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2018-04-02 20:46:37 +0200

    Renamed original Fog to DeferredFog (because it is only deferred effect)
    Added Fog effect which modifies RenderSettings before rendering and restores after rendering

---
+++ PostProcessing/Editor/Effects/FogEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace UnityEditor.Rendering.PostProcessing
+{
+    [PostProcessEditor(typeof(Fog))]
+    public sealed class FogEditor : PostProcessEffectEditor<Fog>
+    {
+        SerializedParameterOverride m_excludeSkybox;
+        SerializedParameterOverride m_color;
+        SerializedParameterOverride m_density;
+        SerializedParameterOverride m_startDistance;
+        SerializedParameterOverride m_endDistance;
+        SerializedParameterOverride m_mode;
+
+        public override void OnEnable()
+        {
+            m_excludeSkybox = FindParameterOverride(x => x.excludeSkybox);
+            m_color = FindParameterOverride(x => x.color);
+            m_density = FindParameterOverride(x => x.density);
+            m_startDistance = FindParameterOverride(x => x.startDistance);
+            m_endDistance = FindParameterOverride(x => x.endDistance);
+            m_mode = FindParameterOverride(x => x.mode);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            PropertyField(m_excludeSkybox);
+            PropertyField(m_color);
+            PropertyField(m_mode);
+            if (m_mode.value.intValue == (int)FogMode.Linear)
+            {
+                PropertyField(m_startDistance);
+                PropertyField(m_endDistance);
+            }
+            else if (m_mode.value.intValue == (int)FogMode.Exponential || m_mode.value.intValue == (int)FogMode.ExponentialSquared)
+            {
+                PropertyField(m_density);
+            }
+        }
+    }
+}
--- PostProcessing/Runtime/Effects/Fog.cs
+++ PostProcessing/Runtime/Effects/Fog.cs
@@ -3,40 +3,88 @@ using System;
 namespace UnityEngine.Rendering.PostProcessing
 {
     [Serializable]
-    public sealed class Fog
+    [PostProcess(typeof(FogRenderer), PostProcessEvent.AfterStack, "Unity/Fog")]
+    public sealed class Fog : PostProcessEffectSettings
     {
-        [Tooltip("Enables the internal deferred fog pass. Actual fog settings should be set in the Lighting panel.")]
-        public bool enabled = true;
+        [Serializable]
+        public sealed class FogModeParameter : ParameterOverride<FogMode> { }
 
-        [Tooltip("Should the fog affect the skybox?")]
-        public bool excludeSkybox = true;
+        public BoolParameter excludeSkybox = new BoolParameter { value = false };
+        public ColorParameter color = new ColorParameter { value = Color.gray };
+        public FloatParameter density = new FloatParameter { value = 0.01f };
+        public FloatParameter startDistance = new FloatParameter { value = 100 };
+        public FloatParameter endDistance = new FloatParameter { value = 200 };
+        public FogModeParameter mode = new FogModeParameter { value = FogMode.Exponential };
 
-        internal DepthTextureMode GetCameraFlags()
+        public override bool IsEnabledAndSupported(PostProcessRenderContext context)
         {
-            return DepthTextureMode.Depth;
+            // Never rendered directly
+            return false;
         }
 
-        internal bool IsEnabledAndSupported(PostProcessRenderContext context)
+        public override void Reset(PostProcessEffectSettings defaultSettings)
         {
-            return enabled
-                && RenderSettings.fog
-                && !RuntimeUtilities.scriptableRenderPipelineActive
-                && context.resources.shaders.deferredFog
-                && context.resources.shaders.deferredFog.isSupported
-                && context.camera.actualRenderingPath == RenderingPath.DeferredShading;  // In forward fog is already done at shader level
+            base.Reset(defaultSettings);
+
+            enabled.value = RenderSettings.fog;
+            color.value = RenderSettings.fogColor;
+            density.value = RenderSettings.fogDensity;
+            startDistance.value = RenderSettings.fogStartDistance;
+            endDistance.value = RenderSettings.fogEndDistance;
+            mode.value = RenderSettings.fogMode;
+        }
+    }
+
+    public sealed class FogRenderer : PostProcessEffectRenderer<Fog>
+    {
+        Fog m_backupSettings = ScriptableObject.CreateInstance<Fog>();
+
+        public override void Render(PostProcessRenderContext context)
+        {
+            throw new NotSupportedException();
         }
 
-        internal void Render(PostProcessRenderContext context)
+        static void Load(Fog settings, ref bool excludeSkybox)
         {
-            var sheet = context.propertySheets.Get(context.resources.shaders.deferredFog);
-            sheet.ClearKeywords();
+            excludeSkybox = settings.excludeSkybox;
+            RenderSettings.fog = settings.enabled;
+            RenderSettings.fogColor = settings.color;
+            RenderSettings.fogDensity = settings.density;
+            RenderSettings.fogStartDistance = settings.startDistance;
+            RenderSettings.fogEndDistance = settings.endDistance;
+            RenderSettings.fogMode = settings.mode;
+        }
 
-            var fogColor = RuntimeUtilities.isLinearColorSpace ? RenderSettings.fogColor.linear : RenderSettings.fogColor;
-            sheet.properties.SetVector(ShaderIDs.FogColor, fogColor);
-            sheet.properties.SetVector(ShaderIDs.FogParams, new Vector3(RenderSettings.fogDensity, RenderSettings.fogStartDistance, RenderSettings.fogEndDistance));
+        static void Store(Fog result, bool excludeSkybox)
+        {
+            result.excludeSkybox.value = excludeSkybox;
+            result.enabled.value = RenderSettings.fog;
+            result.color.value = RenderSettings.fogColor;
+            result.density.value = RenderSettings.fogDensity;
+            result.startDistance.value = RenderSettings.fogStartDistance;
+            result.endDistance.value = RenderSettings.fogEndDistance;
+            result.mode.value = RenderSettings.fogMode;
+        }
 
-            var cmd = context.command;
-            cmd.BlitFullscreenTriangle(context.source, context.destination, sheet, excludeSkybox ? 1 : 0);
+        public void ApplySettings(PostProcessRenderContext context, ref bool excludeSkybox)
+        {
+            // Backup settings
+            Store(m_backupSettings, excludeSkybox);
+
+            // Apply settings
+            Load(settings, ref excludeSkybox);
+
+#if UNITY_EDITOR
+            if (context.isSceneView && !UnityEditor.SceneView.currentDrawingSceneView.sceneViewState.showFog)
+            {
+                RenderSettings.fog = false;
+            }
+#endif
+        }
+
+        public void RestoreSettings(ref bool excludeSkybox)
+        {
+            Load(m_backupSettings, ref excludeSkybox);
         }
     }
 }
