1e4d5e87227dfff4aee4ea173198be67d7e13a9b
PaulDunning <pauldunning@unity3d.com>
2018-04-13 15:40:29 +0100

    Remove UNITY_3D_TEXTURES_SUPPORTED define and use tex2D instead.
    For Vita define the TEXTURE3D related macros to use 2D instead, avoids compiler errors and cleaner code with fewer #if's

--- PostProcessing/Shaders/API/PSP2.hlsl
+++ PostProcessing/Shaders/API/PSP2.hlsl
@@ -2,34 +2,32 @@
 #define UNITY_UV_STARTS_AT_TOP 1
 #define UNITY_REVERSED_Z 0
 #define UNITY_GATHER_SUPPORTED 0
-#define UNITY_3D_TEXTURES_SUPPORTED 0
 #define UNITY_CAN_READ_POSITION_IN_FRAGMENT_PROGRAM 0
 
 #define TEXTURE2D_SAMPLER2D(textureName, samplerName) sampler2D textureName
-#define TEXTURE3D_SAMPLER3D(textureName, samplerName) sampler3D textureName
 
 #define TEXTURE2D(textureName) sampler2D textureName
 #define SAMPLER2D(samplerName)
 
-#define TEXTURE3D(textureName) sampler3D textureName
-#define SAMPLER3D(samplerName)
-
 #define TEXTURE2D_ARGS(textureName, samplerName) sampler2D textureName
 #define TEXTURE2D_PARAM(textureName, samplerName) textureName
 
-#define TEXTURE3D_ARGS(textureName, samplerName) sampler3D textureName
-#define TEXTURE3D_PARAM(textureName, samplerName) textureName
-
 #define SAMPLE_TEXTURE2D(textureName, samplerName, coord2) tex2D(textureName, coord2)
 #define SAMPLE_TEXTURE2D_LOD(textureName, samplerName, coord2, lod) tex2Dlod(textureName, float4(coord2, 0.0, lod))
-
-#define SAMPLE_TEXTURE3D(textureName, samplerName, coord3) tex3D(textureName, coord3)
 
 #define LOAD_TEXTURE2D(textureName, texelSize, icoord2) tex2D(textureName, icoord2 / texelSize)
 #define LOAD_TEXTURE2D_LOD(textureName, texelSize, icoord2) tex2Dlod(textureName, float4(icoord2 / texelSize, 0.0, lod))
 
 #define SAMPLE_DEPTH_TEXTURE(textureName, samplerName, coord2) tex2D<float>(textureName, coord2).r
 #define SAMPLE_DEPTH_TEXTURE_LOD(textureName, samplerName, coord2, lod) tex2Dlod<float>(textureName, float4(coord2, 0.0, lod)).r
+
+// 3D textures are not supported on Vita, use 2D to avoid compile errors.
+#define TEXTURE3D_SAMPLER3D(textureName, samplerName) sampler2D textureName
+#define TEXTURE3D(textureName) sampler2D textureName
+#define SAMPLER3D(samplerName)
+#define TEXTURE3D_ARGS(textureName, samplerName) sampler2D textureName
+#define TEXTURE3D_PARAM(textureName, samplerName) textureName
+#define SAMPLE_TEXTURE3D(textureName, samplerName, coord3) tex2D(textureName, coord3)
 
 #define UNITY_BRANCH
 #define UNITY_FLATTEN
