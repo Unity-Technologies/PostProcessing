73e5dc99e53073942ade7c7ab6d52c62f573049a
Pierre GAC <p.gac@dec-industrie.com>
2021-03-03 16:58:01 +0100

    Added changes from https://github.com/Unity-Technologies/Graphics/commit/3806572801876d4c6191216bb9cab04256ef7c0b#diff-4646e2506088c41089376afacad4fb00432d3b1d463170bf9106c009b68b514e

--- PostProcessing/Runtime/Effects/AmbientOcclusion.cs
+++ PostProcessing/Runtime/Effects/AmbientOcclusion.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine.Experimental.Rendering;
 
 namespace UnityEngine.Rendering.PostProcessing
 {
@@ -189,9 +190,9 @@ namespace UnityEngine.Rendering.PostProcessing
 
                 state &= SystemInfo.supportsComputeShaders
                       && !RuntimeUtilities.isAndroidOpenGL
-                      && RenderTextureFormat.RFloat.IsSupported()
-                      && RenderTextureFormat.RHalf.IsSupported()
-                      && RenderTextureFormat.R8.IsSupported();
+                      && SystemInfo.IsFormatSupported(GraphicsFormat.R32_SFloat, FormatUsage.Render | FormatUsage.LoadStore)
+                      && SystemInfo.IsFormatSupported(GraphicsFormat.R16_SFloat, FormatUsage.Render | FormatUsage.LoadStore)
+                      && SystemInfo.IsFormatSupported(GraphicsFormat.R8_UNorm, FormatUsage.Render | FormatUsage.LoadStore);
             }
 
             return state;
--- PostProcessing/Shaders/Builtins/FinalPass.shader
+++ PostProcessing/Shaders/Builtins/FinalPass.shader
@@ -131,10 +131,10 @@ Shader "Hidden/PostProcessing/FinalPass"
         Pass
         {
             HLSLPROGRAM
-                #pragma only_renderers gles
+                #pragma only_renderers gles3
 
                 #pragma multi_compile __ STEREO_INSTANCING_ENABLED STEREO_DOUBLEWIDE_TARGET
-                #pragma target es3.0
+                #pragma target 3.0
 
             ENDHLSL
         }
